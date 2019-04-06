63f0fc0551b2ba0b7ed82a3bfde3aefa2e266a77
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2019-04-06 14:21:36 +0200

    EM parameter update working under stress_test one case example

--- python/base.py
+++ python/base.py
@@ -3,10 +3,11 @@ from sklearn.base import _pprint
 from scipy.misc import logsumexp
 import numpy as np
 import pandas as pd
-import utils, warnings, sys
+import utils, warnings, sys, time, string
 
 
 class ConvergenceMonitor:
+    _header = '{:>10s} {:>16s} {:>16s}'.format('iteration', 'logprob', 'delta')
     _template = "{iter:>10d} {logprob:>16.4f} {delta:>+16.4f}"
 
     def __init__(self, tol, n_iter, verbose):
@@ -15,6 +16,7 @@ class ConvergenceMonitor:
         self.verbose = verbose
         self.history = deque(maxlen=2)
         self.iter = 0
+        self.logger = utils.make_logger(self.__class__.__name__)
 
     def __repr__(self):
         class_name = self.__class__.__name__
@@ -28,6 +30,8 @@ class ConvergenceMonitor:
     def report(self, logprob):
         if self.verbose:
             delta = logprob - self.history[-1] if self.history else np.nan
+            if delta == np.nan:
+                print(self._header)
             message = self._template.format(iter=self.iter + 1, logprob=logprob, delta=delta)
             print(message, file=sys.stderr)
 
@@ -40,41 +44,36 @@ class ConvergenceMonitor:
 
     @property
     def converged(self):
-        return (self.iter == self.n_iter or 
-                (len(self.history) == 2 and
-                 self.history[1] - self.history[0] < self.tol))
+        converged_ = (self.iter == self.n_iter or 
+                     (len(self.history) == 2 and
+                     self.history[1] - self.history[0] < self.tol))
+        return converged_
 
 
-class HMMConf(utils.Logged):
-    def __init__(self, startprob, transcube, emitmat, confmat, distmat, statemap, obsmap, 
-                 n_states, n_iter=10, tol=1e-2, verbose=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class HMMConf:
+    FIRST_IND = 0
+    SECOND_IND = 1
+    UPDATED_IND = 2
 
-        msg = """Number of {item} are different between state transition cube and emission matrix
+    def __init__(self, startprob, transcube, emitmat, confmat, distmat, 
+                 int2state, int2obs, n_states, n_obs, params='to',
+                 n_iter=10, tol=1e-2, verbose=False, *args, **kwargs): 
+        utils.assert_shape('activities', transcube.shape[0], emitmat.shape[1])
+        utils.assert_shape('states', transcube.shape[1], emitmat.shape[0])
 
-        [transcube]: {left}
-        [emitmat]: {right}
-        """
-
-        obs_msg = msg.format(item='activities', left=transcube.shape[0], right=emitmat.shape[1])
-        state_msg = msg.format(item='states', left=transcube.shape[1], right=emitmat.shape[0])
-        assert transcube.shape[0] == emitmat.shape[1], obs_msg
-        assert transcube.shape[1] == emitmat.shape[0], state_msg
-
+        self.logger = utils.make_logger(self.__class__.__name__)
+        self.params = params
         self.startprob = startprob
         self.transcube = transcube
         self.transcube_d = np.zeros(transcube.shape) + 1. / self.transcube.shape[1]
         self.emitmat = emitmat
         self.emitmat_d = np.zeros(emitmat.shape) + 1. / self.emitmat.shape[1]
-        # self.logtranscube = utils.log_mask_zero(self.transcube)
-        # self.logtranscube_d = utils.log_mask_zero(self.trans_cube_d)
-        # self.logemitmat = utils.log_mask_zero(self.emitmat)
-        # self.logemitmat_d = utils.log_mask_zero(self.emitmat_d)
         self.confmat = confmat
         self.distmat = distmat
-        self.statemap = statemap
-        self.obsmap = obsmap
+        self.int2state = int2state
+        self.int2obs = int2obs
         self.n_states = n_states
+        self.n_obs = n_obs
         self.n_iter = n_iter
         self.tol = tol
         self.verbose = verbose
@@ -101,6 +100,8 @@ class HMMConf(utils.Logged):
         :param obs: observation at time t
         :param conf: conformance between stateprob and obs
         """
+        self.logger.debug('conform: {}'.format(conf))
+        self.logger.debug('emitmat_d: {}'.format(self.emitmat_d[:,obs]))
         prob = conf * self.emitmat[:,obs] + (1 - conf) * self.emitmat_d[:,obs]
         return prob
 
@@ -115,7 +116,7 @@ class HMMConf(utils.Logged):
         prob = conf * self.transcube[obs,:,:] + (1 - conf) * self.transcube_d[obs,:,:]
         return prob
 
-    def forward(self, obs, prev_obs=None, prev_fwd=None):
+    def _forward(self, obs, prev_obs=None, prev_fwd=None):
         """Computes the log forward probability.
 
         :return: log forward probability, conformance array
@@ -123,15 +124,18 @@ class HMMConf(utils.Logged):
         conf_arr = np.full(3, -1.)
 
         if prev_fwd is None:
+            self.logger.debug('startprob: {}'.format(self.startprob))
             emitconf = self.conform(self.startprob, obs)
-            emitprob = self.emissionprob(obs, emitconf)
-            logfwd = utils.log_mask_zero(self.startprob) + utils.log_mask_zero(emitprob)
+            obsprob = self.emissionprob(obs, emitconf)
+            logobsprob = utils.log_mask_zero(obsprob)
+            logfwd = utils.log_mask_zero(self.startprob) + logobsprob
             fwd = logfwd.copy()
             utils.log_normalize(fwd, axis=1)
             fwd = np.exp(fwd)
-            conf_arr[1] = emitconf[0]
-            conf_arr[2] = self.conform(fwd, obs)
-            return logfwd, conf_arr
+            conf_arr[self.SECOND_IND] = emitconf[0]
+            conf_arr[self.UPDATED_IND] = self.conform(fwd, obs)
+            logstateprob = np.full((self.transcube.shape[1], self.transcube.shape[1]), -np.inf)
+            return logfwd, conf_arr, logstateprob, logobsprob
 
         arr_buffer = prev_fwd.copy()
         utils.log_normalize(arr_buffer, axis=1)
@@ -141,7 +145,7 @@ class HMMConf(utils.Logged):
         stateprob = self.stateprob(prev_obs, stateconf)
         logstateprob = utils.log_mask_zero(stateprob)
         # print('Previous fwd: {}'.format(prev_fwd))
-        self.logger.info('State trans probability: \n{}'.format(stateprob))
+        self.logger.debug('State trans probability: \n{}'.format(stateprob))
         cur_fwd_est = logsumexp(logstateprob.T + prev_fwd, axis=1)
         cur_fwd_est = cur_fwd_est.reshape([1, self.n_states])
         # print('Current fwd est.: {}'.format(work_buffer))
@@ -154,27 +158,32 @@ class HMMConf(utils.Logged):
 
         msg0 = '   Log state estimate of time t before observation at time t: {}'.format(cur_fwd_est)
         msg1 = 'W. State estimate of time t before observation at time t: {}'.format(cur_stateprob)
-        self.logger.info(msg0)
-        self.logger.info(msg1)
+        self.logger.debug(msg0)
+        self.logger.debug(msg1)
 
         emitconf = self.conform(cur_stateprob, obs)
         obsprob = self.emissionprob(obs, emitconf)
+        logobsprob = utils.log_mask_zero(obsprob)
+
         msg2 = 'Likelihood of observation at states time t: {}'.format(obsprob)
-        obsprob = utils.log_mask_zero(obsprob)
-        self.logger.info(msg2)
+        self.logger.debug(msg2)
         msg3 = 'Conformance between state and observation at time t ' \
               'before observation adjustment: {:.2f}'.format(emitconf[0])
-        self.logger.info(msg3)
+        self.logger.debug(msg3)
 
-        logfwd = obsprob + cur_fwd_est
+        logfwd = logobsprob + cur_fwd_est
         fwd = logfwd.copy()
         utils.log_normalize(fwd, axis=1)
         fwd = np.exp(fwd)
 
-        conf_arr[0] = stateconf[0]
-        conf_arr[1] = emitconf[0]
-        conf_arr[2] = self.conform(fwd, obs)
+        conf_arr[self.FIRST_IND] = stateconf[0]
+        conf_arr[self.SECOND_IND] = emitconf[0]
+        conf_arr[self.UPDATED_IND] = self.conform(fwd, obs)
 
+        return logfwd, conf_arr, logstateprob, logobsprob
+
+    def forward(self, obs, prev_obs=None, prev_fwd=None):
+        logfwd, conf_arr, _, _ = self._forward(obs, prev_obs, prev_fwd)
         return logfwd, conf_arr
 
     def backward(self, obs, prev_obs, conf_arr, prev_bwd=None):
@@ -190,13 +199,13 @@ class HMMConf(utils.Logged):
         # no need to transpose logstateprob since we are broadcasting addition across the rows
         summed = logemitprob + logstateprob
         if prev_bwd is None:
-            bwd = logsumexp(logemitprob + logstateprob, axis=1)
+            logbwd = logsumexp(logemitprob + logstateprob, axis=1)
         else:
-            bwd = logsumexp(logemitprob + logstateprob + prev_bwd, axis=1)
+            logbwd = logsumexp(logemitprob + logstateprob + prev_bwd, axis=1)
 
-        self.logger.info('Backward probability: \n{}'.format(bwd))
+        self.logger.debug('Backward probability: \n{}'.format(logbwd))
 
-        return bwd
+        return logbwd
 
     def _do_forward_pass(self, X):
         """Computes the forward lattice containing the forward probability of a single sequence of
@@ -204,32 +213,54 @@ class HMMConf(utils.Logged):
 
         :param X: array of observations
         :type X: array_like (n_samples, 1)
-        :return: the forward lattice and the conformance lattice
+        :return: the forward lattice, the conformance lattice, state-transition and observation lattice
         """
         n_samples = X.shape[0]
         fwdlattice = np.ndarray((n_samples, self.n_states))
         conflattice = np.ndarray((n_samples, 3))
+        framelogstateprob = np.ndarray((n_samples, self.n_states, self.n_states))
+        framelogobsprob = np.ndarray((n_samples, self.n_states))
+
+        times = []
 
         # first observation
         obs = X[0,0]
-        fwd, conf = self.forward(obs)
+        start = time.time()
+        fwd, conf, _, logobsprob = self._forward(obs)
+        end = time.time()
+        times.append(end - start)
         fwdlattice[0] = fwd
         conflattice[0] = conf
+        framelogstateprob[0] = -1.
+        framelogobsprob[0] = logobsprob
 
         prev_obs = obs
         prev_fwd = fwd
         for i in range(1, n_samples):
             obs = X[i,0]
-            fwd, conf = self.forward(obs, prev_obs, prev_fwd)
+            start = time.time()
+            fwd, conf, logstateprob, logobsprob = self._forward(obs, prev_obs, prev_fwd)
+            end = time.time()
+            times.append(end - start)
             fwdlattice[i] = fwd
             conflattice[i] = conf
+            framelogstateprob[i] = logstateprob
+            framelogobsprob[i] = logobsprob
 
             prev_obs = obs
             prev_fwd = fwd
 
-        return fwdlattice, conflattice
+        self.logger.debug('Average forward probability time: {:.4f}s'.format(np.mean(times)))
 
-    def _do_backward_pass(self, X, conflattice):
+        with np.errstate(under='ignore'):
+            logprob = logsumexp(fwdlattice[-1])
+            return logprob, fwdlattice, conflattice, framelogstateprob, framelogobsprob
+
+    def do_forward_pass(self, X):
+        logprob, fwdlattice, conflattice, logstateprob, logobsprob = self._do_forward_pass(X)
+        return logprob, fwdlattice, conflattice
+
+    def do_backward_pass(self, X, conflattice):
         """Computes the backward lattice containing the backward probability of a single sequence 
         of observations.
 
@@ -258,10 +289,130 @@ class HMMConf(utils.Logged):
         return bwdlattice
 
     def fit(self, X, lengths):
-        pass
+        self.monitor._reset()
+
+        for it in range(self.n_iter):
+            cur_logprob = 0
+            stats = self._initialize_sufficient_statistics()
+
+            for i, j in utils.iter_from_X_lengths(X, lengths):
+                logprob, fwdlattice, conflattice, logstateprob, logobsprob = self._do_forward_pass(X[i:j])
+                cur_logprob += logprob
+                bwdlattice = self.do_backward_pass(X[i:j], conflattice)
+                posteriors = self._compute_posteriors(fwdlattice, bwdlattice)
+
+                self._accumulate_sufficient_statistics(stats, X[i:j], logstateprob, logobsprob, conflattice,
+                                                       posteriors, fwdlattice, bwdlattice)
+
+            self._do_mstep(stats)
+
+            self.monitor.report(cur_logprob)
+            if self.monitor.converged:
+                msg = 'Converged at iteration {} with current logprob {:.2f} and previous logprob {:.2f}'
+                cur_logprob = self.monitor.history[1] if len(self.monitor.history) > 1 else self.monitor.history[0]
+                prev_logprob = self.monitor.history[1] if len(self.monitor.history) > 1 else -1
+                msg = msg.format(it, cur_logprob, prev_logprob)
+                self.logger.debug(msg)
+                break
+
+        return self
+
+    def _accumulate_sufficient_statistics(self, stats, X, logstateprob, logobsprob, conflattice, 
+                                          posteriors, fwdlattice, bwdlattice):
+        """Updates sufficient statistics from a given sample.
+
+        """
+        if 's' in self.params:
+            stats['start'] += posteriors[0]
+
+        if 't' in self.params:
+            n_samples = logobsprob.shape[0]
+
+            if n_samples <= 1:
+                return
+
+            n_states, n_obs = self.n_states, self.n_obs
+
+            utils.assert_shape('logstateprob', (n_samples, n_states, n_states), logstateprob.shape)
+            utils.assert_shape('logobsprob', (n_samples, n_states), logobsprob.shape)
+
+            log_xi_sum = np.full((self.n_obs, self.n_states, self.n_states), -np.inf)
+
+            for t in range(n_samples - 1):
+                # skip events that are perfectly conforming
+                if conflattice[t, self.UPDATED_IND] >= 1.:  
+                    continue
+
+                o = X[t]    # to identify the state transition matrix to update
+                for i in range(self.n_states):
+                    for j in range(self.n_states):
+                        to_add = (fwdlattice[t, i] + logstateprob[t, i, j]
+                                  + logobsprob[t + 1, j] + bwdlattice[t + 1, j])
+                        log_xi_sum[o, i, j] = np.logaddexp(log_xi_sum[o, i, j], to_add)
+
+            denominator = logsumexp(log_xi_sum, axis=2)
+
+            utils.assert_shape('denominator', (n_obs, n_states), denominator.shape)
+
+            for o in range(self.n_obs):
+                # only update values if sample affected the state-transition between i and j for o
+                to_update = denominator[o,:] != -np.inf
+                # log_xi_sum[o,:] = np.subtract(log_xi_sum[o,:], denominator[o,:], where=to_update)
+                np.subtract(log_xi_sum[o,:], denominator[o,:], out=log_xi_sum[o,:], where=to_update)
+
+            with np.errstate(under='ignore'):
+                stats['trans'] += np.exp(log_xi_sum)
+
+        if 'o' in self.params:
+            n_samples = logobsprob.shape[0]
+
+            i = 0
+            for t, symbol in enumerate(np.concatenate(X)):
+                # skip if it's perfectly conforming
+                if conflattice[t, self.UPDATED_IND] >= 1.:
+                    continue
+
+                self.logger.debug('No. of non-zeros: {}'.format(np.count_nonzero(posteriors[t])))
+                stats['obs'][:, symbol] += posteriors[t]
+                i += 1
+
+            self.logger.debug('There were {} non-conforming events'.format(i))
+
+            denominator = stats['obs'].sum(axis=1)[:, np.newaxis]
+            # avoid zero division by replacement as 1.
+            # denominator[denominator == 0.] = 1.
+            # stats['obs'] /= denominator
+
+            # only update matrix if the sample affected the emission probability
+            to_update = denominator != 0.
+            np.divide(stats['obs'], denominator, out=stats['obs'], where=to_update)
+
+    def _initialize_sufficient_statistics(self):
+        stats = {'nobs': 0,
+                 'start': np.zeros(self.n_states),
+                 'trans': np.zeros(self.transcube.shape),
+                 'obs': np.zeros(self.emitmat.shape)}
+        return stats
+
+    def _do_mstep(self, stats):
+        if 's' in self.params:
+            startprob = self.startprob + stats['start']
+            self.startprob = np.where(self.startprob == 0.,
+                                      self.startprob, startprob)
+            utils.normalize(self.startprob, axis=1)
+
+        if 't' in self.params:
+            # self.transcube_d = np.where(self.transcube_d == 0.,
+            #                             self.transcube_d, transcube)
+            self.transcube_d = stats['trans'] + self.transcube_d
+            utils.normalize(self.transcube_d, axis=2)
+
+        if 'o' in self.params:
+            self.emitmat_d = stats['obs'] + self.emitmat_d
+            utils.normalize(self.emitmat_d, axis=1)
 
     def _compute_posteriors(self, fwdlattice, bwdlattice):
         log_gamma = fwdlattice + bwdlattice
-        utils.log_normalize(log_gamma, axis=1)
+        utils.log_normalize(log_gamma, axis=1)  # this prevents underflow
         with np.errstate(under='ignore'):
-            return np.exp(log_gamma), np.exp(log_gamma).sum(axis=1)
+            return np.exp(log_gamma)
---
+++ python/stress_test.py
@@ -0,0 +1,138 @@
+import numpy as np
+import pandas as pd
+import time, os
+
+import base, slac_setup, pm_extra, tracker
+from pm4py.objects.petri.importer import pnml as pnml_importer
+
+
+MODEL_FP = os.path.join('..', 'data', 'BPM2018', 'stress-test', 'model.pnml')
+DATA_FP = os.path.join('..', 'data', 'BPM2018', 'stress-test', 'filtered-stream.csv')
+
+ACTIVITY = 'activity'
+ACTIVITY_ID = 'activity_id'
+CASEID = 'caseid'
+
+
+np.set_printoptions(precision=2)
+
+
+def import_data():
+    net, init_marking, final_marking = pnml_importer.import_net(MODEL_FP)
+    # print('Number of transitions: {}'.format(len(net.transitions)))
+    data_df = pd.read_csv(DATA_FP)
+    # print(data_df.head())
+    return net, init_marking, final_marking, data_df
+
+
+def map_net_activity(net, actmap):
+    for t in net.transitions:
+        if t.label:
+            t.label = actmap[t.label]
+
+
+def modify_place_name(net):
+    """Append p to place names so that regular expression can be used to map 
+    markings back and forth to its integer representation.
+    """
+    for p in net.places:
+        p.name = 'p' + p.name
+
+
+def process_net(net, init_marking, final_marking):
+    is_inv = lambda t: t.label is None
+    inv_trans = list(filter(is_inv, net.transitions))
+    # print('Number of invisible transitions: {}'.format(len(inv_trans)))
+    rg, inv_states = pm_extra.build_reachability_graph(net, init_marking, is_inv)
+    pm_extra.collapse_inv_trans(rg, inv_states)
+    return rg
+
+
+def setup_hmm(rg):
+    G, node_map = slac_setup.rg_to_nx_undirected(rg, map_nodes=True)
+    dist_df = slac_setup.compute_distance_matrix(G, node_map, as_dataframe=True)
+    distmat = dist_df.values
+    # print('Distance df: \n{}'.format(dist_df))
+
+    obsmap = {t.name: int(t.name) for t in rg.transitions}
+    int2state = {val:key for key, val in node_map.items()}
+    int2obs = {val:key for key, val in obsmap.items()}
+    n_obs = len(obsmap)
+    n_states = len(node_map)
+
+    transcube = slac_setup.compute_state_trans_cube(rg, node_map, obsmap, n_obs, n_states)
+    emitmat = slac_setup.compute_emission_mat(rg, node_map, obsmap, n_obs, n_states)
+    confmat = slac_setup.compute_conformance_mat(emitmat)
+    startprob = slac_setup.compute_startprob(rg, node_map, n_states)
+
+    hmm = base.HMMConf(startprob, transcube, emitmat, confmat, distmat, 
+                       int2state, int2obs, n_states, n_obs, 
+                       params='to', verbose=True)
+    return hmm
+
+
+def make_conformance_tracker(hmm):
+    return tracker.ConformanceTracker(hmm)
+
+
+def event_df_to_hmm_format(df):
+    lengths = df.groupby(CASEID).count()[ACTIVITY_ID].values
+    X = df[[ACTIVITY_ID]].values
+    return X, lengths
+
+
+if __name__ == '__main__':
+    print('Start stress test...')
+    start = time.time()
+
+    print('Importing data...')
+    net, init_marking, final_marking, data_df = import_data()
+
+    print('Mapping activity to integer labels...')
+    actmap = data_df[[ACTIVITY, ACTIVITY_ID]].set_index(ACTIVITY).to_dict()[ACTIVITY_ID]
+    rev_actmap = {key:val for key, val in actmap.items()}
+    map_net_activity(net, actmap)
+
+    print('Modify place names...')
+    modify_place_name(net)
+
+    print('Process net...')
+    rg = process_net(net, init_marking, final_marking)
+
+    print('Setting up HMM...')
+    hmm = setup_hmm(rg)
+
+    print('Make conformance tracker...')
+    tracker = make_conformance_tracker(hmm)
+
+    caseids = data_df[CASEID].unique()[:10]
+    caseids = ['case_34'] # warm start example
+    to_include = data_df[CASEID].isin(caseids)
+    filtered_df = data_df.loc[to_include,:]
+
+    print('data df shape: {}'.format(filtered_df.shape))
+
+    # EM training
+    print('EMing...')
+    X, lengths = event_df_to_hmm_format(filtered_df)
+    tracker.hmm.fit(X, lengths)
+
+    i = 0
+    limit = 1000
+    for row in filtered_df.itertuples(index=False):
+        caseid = row.caseid
+        event = row.activity_id
+        act = row.activity
+
+        score, state = tracker.replay_event(caseid, event)
+        print('{} replay {:<11}: {:.2f}, {}'.format(caseid, act, score[2], state))
+
+        # time.sleep(0.1)
+
+        i += 1
+        if i > limit:
+            break
+
+    end = time.time()
+    took = end - start
+    print('Stress test took {:.2f}s'.format(took))
